c978f89a17a25f15670f543dcb88f0e59209b99d
pwangy <blipsandclicks@gmail.com>
2024-02-02 15:51:31 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+const renderBooks = (books) => {
+	const main = document.querySelector('main')
+
+	books.forEach((book) => {
+		const h2 = document.createElement('h2')
+		h2.innerHTML = book.name
+		main.appendChild(h2)
+	})
+}
+
+const fetchBooks = () => {
+	const api = 'https://anapioficeandfire.com/api/'
   
+  return fetch(`${api}books`)
+		.then(res => res.json())
+		.then(books => renderBooks(books))
+		.catch(err => console.error(err))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+document.addEventListener('DOMContentLoaded', function () {
+	fetchBooks()
 })
